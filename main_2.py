29c0366f11daefc42116326724284d71e3952737
David Yrigoyen <dave@dryweb.com>
2023-03-02 09:26:03 -0800

    Refactored

--- main_2.py
+++ main_2.py
@@ -1,34 +1,32 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-import sys
 
-machine_on = True
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_maker = MoneyMachine()
 espresso = MenuItem("Espresso", 50, 0, 18, 1.5)
 latte = MenuItem("Espresso", 200, 150, 24, 2.5)
 cappuccino = MenuItem("Espresso", 250, 100, 24, 3.0)
-m = Menu()
-cm = CoffeeMaker()
-mm = MoneyMachine()
 
 
-def str_to_class(choice):
-    return getattr(sys.modules[__name__], choice)
+machine_on = True
 
 
 def process_selection(choice):
-    if choice == "report":
-        cm.report()
-        mm.report()
-    elif choice == "espresso" or choice == "latte" or choice == "cappuccino":
-        drink_choice = str_to_class(choice)
-        if cm.is_resource_sufficient(drink_choice):
-            if mm.make_payment(drink_choice.cost):
-                cm.make_coffee(drink_choice)
-    elif choice == "off":
+    if choice == "off":
         global machine_on
         machine_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_maker.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
 
 
 while machine_on:
-    process_selection(input("What would you like? (espresso/latte/cappuccino): ").lower())
+    options = menu.get_items()
+    process_selection(input(f"What would you like? ({options}): ").lower())
